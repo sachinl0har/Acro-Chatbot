cfce38cbe254add0757692aa7bd9a28b51164ba0
sachinl0har <sachin.l0har0424@gmail.com>
2022-02-12 22:44:33 +0530

    Update

--- AcroBot/settings.py
+++ AcroBot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y5noiq#ua3sp)91klbeqklr(*%w)q4+cupy-7_eud)_%#3mu%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'https://acro-bot.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
