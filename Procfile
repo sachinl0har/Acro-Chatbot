7102ccd3653c43e170e087c631c5b1f97b4d31c0
sachinl0har <sachin.l0har0424@gmail.com>
2022-02-13 09:40:15 +0530

    Delete

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn AcroBot.wsgi
--- base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Message
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Message)
-
-admin.site.site_title = 'AcroBot Admin Panel'
-admin.site.site_header = 'AcroBot Admin Panel'
